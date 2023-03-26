342850692f4ea80188ebb73f1b7ce57f7020f9a7
linaadamson <Adamsonlina@gmail.com>
2023-03-26 22:10:51 +0100

    Feat: converted navbar css to scss

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -3,104 +3,43 @@ import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 // STYLES
-import "./Navbar.css";
+import classes from "./Navbar.module.scss";
 
 // IMAGE IMPORT
 import Logo from "../../assets/logo.svg";
 
-// mobile navbar icon
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 export default function Navbar() {
-  // state to check if mobile navbar is open
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
-
-  // mobile navbar toggle on and off
-  const showNav = () => {
-    setIsNavExpanded(!isNavExpanded);
-  };
-
   return (
-    <>
-      <div className="logo">
+    <nav className={classes.Navbar}>
+      <div className={classes.Logo}>
         <Link to="/">
           <img src={Logo} alt="cover bank logo" />
         </Link>
       </div>
-      <nav className="desktop-navbar">
-        <ul>
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? "active" : "inactive")}
-              to="/"
-            >
-              Cover Bank
-            </NavLink>
-          </li>
 
-          <li>
-            <NavLink
-              className={({ isActive }) => (isActive ? "active" : "inactive")}
-              to="/about"
-            >
-              About
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <ul>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${classes.active}` : `${classes.inactive}`
+            }
+            to="/"
+          >
+            Cover Bank
+          </NavLink>
+        </li>
 
-      {/* hamburger */}
-      {isNavExpanded ? (
-        //menu btn
-        <FontAwesomeIcon
-          className="icon hamburger"
-          icon="fa fa-times"
-          aria-hidden="true"
-          onClick={() => {
-            showNav();
-          }}
-        />
-      ) : (
-        <FontAwesomeIcon
-          className="icon bars"
-          icon="fa fa-bars"
-          aria-hidden="true"
-          onClick={() => {
-            showNav();
-          }}
-        />
-      )}
-
-      {isNavExpanded && (
-        <nav className="mobile-navbar">
-          <ul>
-            <li>
-              <p> MENU </p>
-            </li>
-            <li>
-              <NavLink
-                to="/"
-                onClick={() => {
-                  showNav();
-                }}
-              >
-                Cover Bank
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink
-                to="/about"
-                onClick={() => {
-                  showNav();
-                }}
-              >
-                About
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${classes.active}` : `${classes.inactive}`
+            }
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
---
+++ src/components/searchFilter/SelectDropdownStyles.js
@@ -0,0 +1,58 @@
+// SELECT DROPDOWN STYLES
+export const colourStyles = {
+  menuList: (styles) => ({
+    ...styles,
+    background: "#FDFDFD",
+    fontSize: "1.4rem",
+    // minHeight: '35.2rem',
+    "::-webkit-scrollbar": {
+      display: "none",
+    },
+    "::-webkit-scrollbar-track": {
+      display: "none",
+    },
+    "::-webkit-scrollbar-thumb": {
+      display: "none",
+    },
+    "::-webkit-scrollbar-thumb:hover": {
+      display: "none",
+    },
+  }),
+  option: (styles, { isFocused, isSelected }) => ({
+    ...styles,
+    background: isFocused ? "#dedede" : isSelected ? "#683522" : undefined,
+    zIndex: 1,
+  }),
+  menu: (base) => ({
+    ...base,
+    fontSize: "1.4rem",
+    zIndex: 100,
+  }),
+
+  placeholder: (defaultStyles) => {
+    return {
+      ...defaultStyles,
+      fontSize: "1.4rem",
+      // color: "#4C4C4C",
+    };
+  },
+
+  control: (base) => ({
+    ...base,
+    boxShadow: "none",
+    borderColor: "#cccccc",
+    "&:hover": {
+      borderColor: "#cccccc",
+    },
+    height: "4.2rem",
+    width: "27rem",
+    borderRadius: "0.4rem",
+  }),
+
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    // color: "#FFAE12",
+  }),
+  indicatorSeparator: (styles) => ({ display: "none" }),
+  loadingIndicator: (styles) => ({ display: "none" }),
+};
