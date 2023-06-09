aac6c2b0cd7df76f7964937a9a526a300cb2312c
kingebere <gudoski30@gmail.com>
2023-06-09 13:32:46 +0100

    feat:add contributors

--- src/data/Contributors.js
+++ src/data/Contributors.js
@@ -7,6 +7,11 @@ import Ope from "../assets/contributors/ope.webp";
 import Tomiwa from "../assets/contributors/tomiwa.webp";
 import Wale from "../assets/contributors/wale.webp";
 import Lina from "../assets/contributors/lina.svg";
+import Emmanuel from "../assets/contributors/emmanuel.svg";
+import Eseosa from "../assets/contributors/eseosa.svg";
+import Toyosi from "../assets/contributors/toyosi.svg";
+import Jessica from "../assets/contributors/jessica.svg";
+import Goodluck from "../assets/contributors/goodluck.svg";
 
 export const contributors = [
   {
@@ -48,5 +53,30 @@ export const contributors = [
     name: "Wale Olowokerende",
     role: "Writer",
     image: Wale,
+  },
+   {
+    name: "Emmanuel Aminu",
+    role: "Data Analyst",
+    image: Emmanuel,
+  },
+   {
+    name: "Wale Olowokerende",
+    role: "Writer",
+    image: Eseosa,
+  },
+   {
+    name: "Toyosi Odukale",
+    role: "Engineer",
+    image: Toyosi,
+  },
+   {
+    name: "Jessica Loius",
+    role: "Artist",
+    image: Jessica,
+  },
+   {
+    name: "Dike Goodluck",
+    role: "Engineer",
+    image: Goodluck,
   },
 ];
