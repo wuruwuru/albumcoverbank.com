20a0bfbd94acf45904fb7a6b613d4cedd4441855
Opemipo Aikomo <opemipoaikomo@gmail.com>
2023-06-15 07:38:18 +0100

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import classes from "./Footer.module.scss";
 export default function Footer() {
   return (
     <div className={classes.Footer}>
-      <a href="https://wuruwuru.com" target="_blank">&times; wuruwuru</a>
+      <span><a href="https://wuruwuru.com" target="_blank">&times; wuruwuru</a></span>
     </div>
   );
 }
