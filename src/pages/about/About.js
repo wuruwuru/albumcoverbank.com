19137a7b6b09ebb75a0bda51dc28340c8ebcd097
Opemipo Aikomo <opemipoaikomo@gmail.com>
2023-10-15 18:53:43 +0100

    Update About.js

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -14,6 +14,10 @@ export default function About() {
         <p>The main goal of the archive is to further establish the history of Nigerian graphic design. At the core, it’s about cultural identity. The other goal is to highlight album cover artists as important cultural producers. We’ve documented 750 people so far! </p>
         <p>We hope the archive is used a platform for graphic design inspiration and storytelling. Each cover represents a unique person, place, time, which is a valuable tool for enthusiasts, researchers, educators and storytellers.</p>
         <p>The project is built and maintained by a team of contributors.</p>
+        <blockquote>Whether or not you were familiar with Pino and the Heartbeats at the time, going to the record store and stumbling on this album cover meant you were seduced by this promise of a good time. That’s what cover art for albums and singles do: be a good wingman for the music. Nigerian music, now one of the country’s biggest cultural exports, may not need a wingman, but its accompanying visuals definitely expand our aesthetic experience of both the music itself and Nigerian society at large. Nowhere is this more obvious than in Wuruwuru’s cover bank of over 5000 album covers in Nigeria’s music history since 1950. The covers help us see not just the spirit of the music but also the ever-evolving textures of the soft belly skin of the Nigerian milieu. After all, these album covers were the images the people brought home to their living rooms, the images that lived on in the hearts of the people.</blockquote>
+        <p><a href="https://www.waxpoetics.com/article/nigerian-1970s-album-covers-wuruwuru-archives/" target="_blank">Face the Music</a></p>
+        <h4>Contributions</h4>
+        <p>If you'd like to write an essay based on the album cover and get published in global music publications like Wax Poetics, please submit <a href="#">this form</a>.</p>
       </div>
 
       {/* CONTRIBUTORS */}
