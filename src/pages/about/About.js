f7a56a4300c298e5ec6ad5b105c58dba364fb140
Opemipo Aikomo <opemipoaikomo@gmail.com>
2023-10-15 17:22:51 +0100

    Copy+

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -9,11 +9,10 @@ export default function About() {
   return (
     <div className={classes.about}>
       <div className={classes.aboutWrapper}>
-        <h2> About</h2>
-        <p>Cover Bank is a digital archive of Nigerian album cover art from 1950 to date. It provides a unique and comprehensive view of the evolution of Nigerian album art</p>
-        <p>The main goal of the archive is to further establish the history of Nigerian graphic design. At the core, it’s about cultural identity. The other goal is to provide a platform to celebrate album cover artists. We’ve documented 750 designers so far! </p>
-        <p>We hope the archive is used a platform for graphic design inspiration and storytelling. Each cover represents a place, time and person (or group), a valuable tool for enthusiasts, researchers and educators.
-        </p>
+        <h2>About</h2>
+        <p>Album Cover Bank is a digital archive of Nigerian album cover art published from 1950 to date. It provides a unique and comprehensive view into the evolution of Nigeria music and graphic design.</p>
+        <p>The main goal of the archive is to further establish the history of Nigerian graphic design. At the core, it’s about cultural identity. The other goal is to highlight album cover artists as important cultural producers. We’ve documented 750 people so far! </p>
+        <p>We hope the archive is used a platform for graphic design inspiration and storytelling. Each cover represents a unique person, place, time, which is a valuable tool for enthusiasts, researchers, educators and storytellers.</p>
         <p>The project is built and maintained by a team of contributors.</p>
       </div>
 
