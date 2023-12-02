eb50985fc6bd4db0e7ce25449005ab460649ee93
fathermerry <opemipoaikomo@gmail.comclear>
2023-12-02 09:37:37 +0100

    Clean up About page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -9,14 +9,28 @@ export default function About() {
   return (
     <div className={classes.about}>
       <div className={classes.aboutWrapper}>
-        <h2>About</h2>
-        <p>Album Cover Bank is a digital archive of Nigerian album covers published from 1950 to date. It provides a comprehensive view into the evolution of Nigerian music and graphic design.</p>
-        <p>At its core, the project is about cultural identity. The primary goal of the archive is to establish the history of Nigerian graphic design. We're also looking to highlight album cover artists as important cultural producers. So far, we’ve documented 750 designers! </p>
-        <p>The archive is a platform for graphic design and storytelling inspiration. Each cover is a unique window into history, which makes the collection a valuable tool for enthusiasts, researchers, educators and artists of all kind.</p>
-        <p>The project is built and maintained by a team of contributors.</p>
-        <h4>Contributions</h4>
-        <p><a href="https://www.waxpoetics.com/article/nigerian-1970s-album-covers-wuruwuru-archives/" target="_blank">Face the Music</a> is the first essay published from the album cover archive. It's written by multidisciplinary artist <a href="https://immaculataabba.com/" target="_blank">Immaculata Abba</a> and discusses how the covers from 1970 (post-independence) reflected national identities.</p>
-        <p>If you'd like to write an essay like this and get published in global music publications like Wax Poetics, please <a href="#">send a pitch</a>.</p>
+        <section>
+          <h3>About</h3>
+          <p>Cover Bank is a digital archive of Nigerian album covers from 1950 to date. It provides a unique view into the evolution of Nigerian music.</p>
+          <p>At its core, the archive is a research project that hopes to establish the history of Nigerian graphic design and highlight cover artists as important cultural producers.</p>
+          <p>Cover Bank is also a platform for design and storytelling inspiration. Our goal is to become a valuable tool for artists, enthusiasts, educators and researchers of all kind.</p>
+          <p>The project is built by wuruwuru, a collective for independent creators and maintained by a team of contributors.</p>
+        </section>
+        <section>
+          <h3>Essays</h3>
+          <p>
+            <a href="https://www.albumcoverbank.com/funk"><b>Nigerian Funk</b></a> <br></br>
+            <span>Opemipo Aikomo & Sample Chief</span>
+          </p>
+          <p>
+            <a href="https://www.waxpoetics.com/article/nigerian-1970s-album-covers-wuruwuru-archives/"><b>Face the Music</b></a> <br></br>
+            <span>Immaculata Abba & Wax Poetics</span>
+          </p>
+        </section>
+        <section>
+          <h3>Join us</h3>
+          <p>You can contribute the Cover Bank project by helping to maintain the database, working on the website or publishing a story inspired by the covers. If you're interested, please send us an email.</p>
+        </section>
       </div>
 
       {/* CONTRIBUTORS */}
