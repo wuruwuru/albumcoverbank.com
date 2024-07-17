9b0e807e66d84f263376856adcb680f36108f6c8
Opemipo Aikomo <opemipoaikomo@gmail.com>
2024-07-17 04:40:13 +0100

    Independent Arts

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,8 +4,8 @@ import classes from "./Footer.module.scss";
 export default function Footer() {
   return (
     <div className={classes.Footer}>
-      <span><a href="https://wuruwuru.com" target="_blank">wuruwuru</a></span>
-      <span>&copy; 2023</span>
+      <span><a href="https://wuruwuru.com" target="_blank">Independent Arts</a></span>
+      <span>&copy; 2024</span>
     </div>
   );
 }
